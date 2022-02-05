fbb8e0b6081837965fc1714b9b2c9e46b7a79d51
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-02-05 15:25:23 +0200

    docs: add localStorage saving

--- src/App.js
+++ src/App.js
@@ -17,6 +17,20 @@ class App extends Component {
     filter: "",
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = ({ name, number }) => {
     const findName = this.state.contacts.find(
       (contact) => contact.name.toLowerCase() === name.toLowerCase()
--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -15,6 +15,7 @@ class ContactForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.onSubmit(this.state);
+    this.reset();
   };
 
   reset = () => {
