42d1d3b80e354b985e9c049a196013868df93276
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-02-05 14:17:29 +0200

    docs: add basic files

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./App.css";
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+import ContactForm from "./Components/ContactForm/ContactForm";
+import { Section } from "./Components/Section/Section";
+import { ContactList } from "./Components/ContactList/ContactList";
+import { Filter } from "./Components/Filter/Filter";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  addContact = ({ name, number }) => {
+    const findName = this.state.contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (findName) {
+      return alert(`${name} is already in contacts.`);
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [{ id: nanoid(), name, number }, ...contacts],
+    }));
+  };
+
+  getContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+
+  removeContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+
+  handleFilter = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.handleFilter} />
+          <ContactList
+            contacts={this.getContacts()}
+            onRemoveContact={this.removeContact}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+import s from "./ContactForm.module.css";
+import React, { Component } from "react";
+
+class ContactForm extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+  };
+
+  reset = () => {
+    this.setState({ name: "", number: "" });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <>
+        <form onSubmit={this.handleSubmit} className={s.form}>
+          <label className={s.label}>
+            Name
+            <input
+              className={s.input}
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={s.label}>
+            Number
+            <input
+              className={s.input}
+              type="tel"
+              name="number"
+              value={number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit" className={s.button}>
+            Add contact
+          </button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default ContactForm;
